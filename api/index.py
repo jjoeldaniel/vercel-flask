4ccf0534435027f84a69ce9d1520bb1e9a029401
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-19 10:42:57 -0800

    Now handles index route

--- api/index.py
+++ api/index.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
-
